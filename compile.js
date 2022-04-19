3b423960a3564c7e1b7633f922a1731515b80fb4
rskim47 <rickkim95@gmail.com>
2022-04-19 01:02:53 -0500

    testing compile.js

---
+++ compile.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const fs = require('fs');
+const solc = require('solc')
+
+// Accessing the file on harddrive
+const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
+const source = fs.readFileSync(inboxPath, 'utf8');
+
+// Logging function 
+const log = (content) => console.log(content)
+
+log(solc.compile(source, 1));
+
+
+
+
+
