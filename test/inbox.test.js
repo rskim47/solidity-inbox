0914f7301b66914655b5ba0a6662760ae3381c84
rskim47 <rickkim95@gmail.com>
2022-04-19 01:19:44 -0500

    building more test cases

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -1,3 +1,13 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
+
+// Creating instance of Web3
+const web3 = Web3(ganache.provider()); // the provider is a comms layer
+
+beforeEach(() => {
+  // Get a list of all accounts
+  web3.eth.getAccounts();
+
+  // Use 1 account to deploy contract
+});
